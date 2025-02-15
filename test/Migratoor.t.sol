9d3a5a6210bd3a8f7eea432c7bd8d3b7fab39f55
Adrian Sutton <adrian@oplabs.co>
2025-02-16 07:41:40 +1000

    Format again.

--- test/Migratoor.t.sol
+++ test/Migratoor.t.sol
@@ -5,8 +5,6 @@ import {Test, console} from "forge-std/Test.sol";
 import {Migratoor} from "../src/Migratoor.sol";
 
 contract CounterTest is Test {
-
-
     // TODO: Learn to write solidity tests....
     function testNothing() public {}
 }
