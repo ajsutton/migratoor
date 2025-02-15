0abaeec1e43845f431a778ac510f01e152c40202
Adrian Sutton <adrian@oplabs.co>
2025-02-16 07:40:28 +1000

    Test nothing.

--- test/Migratoor.t.sol
+++ test/Migratoor.t.sol
@@ -5,11 +5,8 @@ import {Test, console} from "forge-std/Test.sol";
 import {Migratoor} from "../src/Migratoor.sol";
 
 contract CounterTest is Test {
-    Migratoor public counter;
 
-    function setUp() public {
-        counter = new Migratoor();
-    }
 
     // TODO: Learn to write solidity tests....
+    function testNothing() public {}
 }
